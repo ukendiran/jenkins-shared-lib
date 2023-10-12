40f0de75c5857f0353b62173ab07d5d25f97a3cd
Ukendiran <ukendiran@gmail.com>
2023-10-12 19:21:18 +0530

    Update mySharedLibrary.groovy

--- vars/mySharedLibrary.groovy
+++ vars/mySharedLibrary.groovy
@@ -1,18 +1,23 @@
 // vars/mySharedLibrary.groovy
 
 def mySwitchMethod(value) {
+     def parameters = [:]
     switch (value) {
         case 'case1':
             echo "Performing action for case 1"
-            // Add your actions here
+            parameters['namespace'] = 'fdp-int1'
+            parameters['cluster'] = 'integration1'
             break
         case 'case2':
             echo "Performing action for case 2"
-            // Add your actions here
+            parameters['namespace'] = 'fdp-int2'
+            parameters['cluster'] = 'integration2'
             break
         default:
             echo "Default action for unknown cases"
-            // Add your default actions here
+            parameters['namespace'] = 'fdp-int3'
+            parameters['cluster'] = 'integration3'
             break
     }
+    return parameters
 }
