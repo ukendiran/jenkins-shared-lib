73361eed274be8e5ca7a9c65753404865c72de7d
Ukendiran <ukendiran@gmail.com>
2023-10-12 18:07:19 +0530

    Create gitInfo.groovy

---
+++ vars/gitInfo.groovy
@@ -0,0 +1,7 @@
+def call() {
+    return [
+        git_name: 'petplan-digital/devops-automation',
+        credentialsId: 'git-fdp-prospect-pet-service',
+        url: 'git@bitbucket.org:petplan-digital/devops-automation.git'
+    ]
+}
